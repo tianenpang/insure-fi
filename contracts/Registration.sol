633f96a1525c2011b715d42d5e77d5b9f4eee876
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2022-05-08 09:33:22 -0400

    Update Registration.sol

--- contracts/Registration.sol
+++ contracts/Registration.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.8.4;
 
 contract Registration {
 
+    uint immutable bmw = 0.2 ether;
+    uint immutable mercedes = 0.2 ether;
+    uint immutable audi = 0.2 ether;
+    uint immutable gmc = 0.15 ether;
+    uint immutable chevrolet = 0.15 ether;
+    uint immutable ford = 0.15 ether;
+    uint immutable toyota = 0.10 ether;
+    uint immutable honda = 0.10 ether;
+    uint immutable nissan = 0.10 ether;
+ 
+    // Inputs for car
     struct Car {
         string carMake;
         string carModel;
@@ -20,6 +31,7 @@ contract Registration {
     //Mapping
     mapping(address => Car) private insuree;
 
+    // Function to register car
     function registerCar (string memory _carMake, string memory _carModel, uint16 _carYear, uint16 _mileage, string memory _licensePlate) public {
         Car storage car = insuree[msg.sender];
         car.carMake = _carMake;
@@ -30,10 +42,11 @@ contract Registration {
         car.licensePlate = _licensePlate;
     }
 
-    function viewRegistration () public view returns(Car memory) {
-        return insuree[msg.sender];
-    }
+    // function viewRegistration () public view returns(Car memory) {
+    //     return insuree[msg.sender];
+    // }
 
+    // Function to get price by year
     function getPriceYear() internal returns(uint){
         Car memory car = insuree[msg.sender];
         uint carYear = car.carYear;
@@ -47,30 +60,55 @@ contract Registration {
             return 0.05 ether;
         }
     }
-    // function getPriceMake() internal returns(uint) {
-    //     Car memory car = insuree[msg.sender];
-    //     bytes memory carMake = bytes(car.carMake);
-    //     bytes memory BMW = abi.encodePacked("BMW");
-    //     bytes memory Mercedes = abi.encodePacked("Mercedes"); 
-    //     bytes memory Audi = abi.encodePacked("Audi"); 
-    //     bytes memory GMC = abi.encodePacked("GMC"); 
-    //     bytes memory Chevrolet = abi.encodePacked("Chevrolet"); 
-    //     bytes memory Ford = abi.encodePacked("Ford"); 
-    //     bytes memory Toyota = abi.encodePacked("Toyota"); 
-    //     bytes memory Honda = abi.encodePacked("Honda"); 
-    //     bytes memory Nissan = abi.encodePacked("Nissan"); 
-        
-    //     // if (keccak256(carMake == BMW == Mercedes == Audi)) return 0.2 ether;
-    //     // if (carMake == Mercedes) return 0.2 ether;
-    //     // if (carMake == Audi) return 0.2 ether;
-    //     // if (carMake == GMC) return 0.15 ether;
-    //     // if (carMake == Chevrolet) return 0.15 ether;
-    //     // if (carMake == Ford) return 0.15 ether;
-    //     // if (carMake == Toyota) return 0.1 ether;
-    //     // if (carMake == Honda) return 0.1 ether;
-    //     // if (carMake == Nissan) return 0.1 ether;
+   
+    // Function to get price by make
+    function getPriceMake() internal returns(uint) {
+        Car memory car = insuree[msg.sender];
+        uint price;
 
-    // }
+        bytes8 carMake = bytes8(keccak256(abi.encodePacked(car.carMake)));
+        bytes8 BMW = bytes8(keccak256(abi.encodePacked("BMW")));
+        bytes8 Mercedes = bytes8(keccak256(abi.encodePacked("Mercedes"))); 
+        bytes8 Audi = bytes8(keccak256(abi.encodePacked("Audi"))); 
+        bytes8 GMC = bytes8(keccak256(abi.encodePacked("GMC"))); 
+        bytes8 Chevrolet = bytes8(keccak256(abi.encodePacked("Chevrolet"))); 
+        bytes8 Ford = bytes8(keccak256(abi.encodePacked("Ford"))); 
+        bytes8 Toyota = bytes8(keccak256(abi.encodePacked("Toyota"))); 
+        bytes8 Honda = bytes8(keccak256(abi.encodePacked("Honda"))); 
+        bytes8 Nissan = bytes8(keccak256(abi.encodePacked("Nissan")));
+
+        if (carMake == BMW) {
+            price = bmw;
+        }
+        if (carMake == Mercedes) {
+            price = mercedes;
+        }
+        if (carMake == Audi) {
+            price = audi;
+        }
+        if (carMake == GMC) {
+            price = gmc;
+        }
+        if (carMake == Chevrolet) {
+            price = chevrolet;
+        }
+        if (carMake == Ford) {
+            price = ford;
+        }
+        if (carMake == Toyota) {
+            price = toyota;
+        }
+        if (carMake == Honda) {
+            price = honda;
+        }
+        if (carMake == Nissan) {
+            price = nissan;
+        }
+        return price;
+
+    }
+
+    // Function to get price by mileage
     function getPriceMileage() internal returns(uint) {
         Car memory car = insuree[msg.sender];
         uint mileage = car.mileage;
@@ -85,10 +123,12 @@ contract Registration {
         }
     }
 
+    // Function to get total cost
     function getCost () public returns(uint) {
-        return getPriceMileage() + getPriceYear();
+        return getPriceMileage() + getPriceYear() + getPriceMake();
     }
     
+    // Function to make payment
     function makePayment () public payable {
         require(msg.value == getCost(), "insufficient ether");
 
