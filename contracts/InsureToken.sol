b379e56a90f6a50c6f10fbdb1760d71f85b0d670
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2022-05-18 10:03:21 -0400

    Create InsureToken.sol

---
+++ contracts/InsureToken.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+abstract contract InsureToken is IERC20, ERC20 {
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+        _mint(msg.sender,100000000000000*10**18);
+    }
+}
