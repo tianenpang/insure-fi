f2445ed488dd305f93b3e4b8db7e5be035732e75
TianenPang <32772271+TianenPang@users.noreply.github.com>
2022-05-21 14:25:07 +0800

    fix: hardhat ts support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,18 +8,12 @@ import 'solidity-coverage';
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
@@ -40,6 +34,11 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  typechain: {
+    target: 'ethers-v5',
+    outDir: 'artifacts/contract-type',
+    alwaysGenerateOverloads: false
   }
 };
 
--- test/testClaims.ts
+++ test/testClaims.ts
@@ -1,31 +1,25 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
+import type { Claims } from '@contract-type';
+// import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
-describe("Test Claims", function () {
-    let greeter:any
-    let user1:SignerWithAddress
-    let user2:SignerWithAddress
-    let user3:SignerWithAddress
+describe('Test Claims', function () {
+  let claims: Claims;
+  // let user1: SignerWithAddress;
+  // let user2: SignerWithAddress;
+  // let user3: SignerWithAddress;
 
-    before(
-        async() =>{
-            const Greeter = await ethers.getContractFactory("Claims");
-            greeter = await Greeter.deploy("Hello, world!");
-            await greeter.deployed();
-            [user1,user2,user3] = await ethers.getSigners();
-        }
-    )
+  before(async () => {
+    const Claims = await ethers.getContractFactory('Claims');
+    claims = await Claims.deploy('', '', '', {});
+    await claims.deployed();
+    await ethers.getSigners();
+    // [user1, user2, user3] = await ethers.getSigners();
+  });
   it("Should return the new greeting once it's changed", async function () {
-
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
+    // expect(await claims.greet()).to.equal('Hello, world!');
+    // const setGreetingTx = await claims.setGreeting('Hola, mundo!');
     // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // await setGreetingTx.wait();
+    // expect(await greeter.greet()).to.equal('Hola, mundo!');
   });
 });
